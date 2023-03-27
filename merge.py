b68374288ae9d91d252e82f8a2e960ab6f65604d
cz <chunzhu@hotmail.com>
2023-03-27 18:17:04 +0800

    added merge data

---
+++ merge.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+sic6l = pd.read_csv("C6L.SI.csv")
+print(sic6l)
+hk0293 = pd.read_csv("0293.HK.csv")
+result = sic6l.join(hk0293,how='inner',rsuffix='_hk')
+
+print(result['Close'].pct_change())
+
+result = pd.concat((result, result['Close'].pct_change().rename('pct_change')), axis=1)
+result = pd.concat((result, result['Close_hk'].pct_change().rename('pct_change_hk')), axis=1)
+result.to_csv("merge.csv")
